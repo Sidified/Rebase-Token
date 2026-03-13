3d953d129d68e227580f1e4df2738f94421fd48f
Sid Harry <sidforwork46@gmail.com>
2026-03-13 12:34:07 +0530

    convert chainlink-ccip from submodule to local dependency

---
+++ lib/chainlink-ccip/chains/evm/gobindings/go_generate.go
@@ -0,0 +1,49 @@
+package ccip
+
+//go:generate go run ./wrap ccip Router router
+//go:generate go run ./wrap ccip CCIPHome ccip_home
+//go:generate go run ./wrap ccip OnRamp onramp
+//go:generate go run ./wrap ccip OffRamp offramp
+//go:generate go run ./wrap ccip FeeQuoter fee_quoter
+//go:generate go run ./wrap ccip NonceManager nonce_manager
+//go:generate go run ./wrap ccip TokenAdminRegistry token_admin_registry
+//go:generate go run ./wrap ccip TokenPoolFactory token_pool_factory
+//go:generate go run ./wrap ccip FactoryBurnMintERC20 factory_burn_mint_erc20
+//go:generate go run ./wrap ccip RegistryModuleOwnerCustom registry_module_owner_custom
+//go:generate go run ./wrap ccip RMNProxy rmn_proxy_contract
+//go:generate go run ./wrap ccip RMNRemote rmn_remote
+//go:generate go run ./wrap ccip RMNHome rmn_home
+
+//go:generate go run ./wrap ccip BurnMintTokenPool burn_mint_token_pool
+//go:generate go run ./wrap ccip BurnFromMintTokenPool burn_from_mint_token_pool
+//go:generate go run ./wrap ccip BurnWithFromMintTokenPool burn_with_from_mint_token_pool
+//go:generate go run ./wrap ccip LockReleaseTokenPool lock_release_token_pool
+//go:generate go run ./wrap ccip TokenPool token_pool
+//go:generate go run ./wrap ccip SiloedLockReleaseTokenPool siloed_lock_release_token_pool
+//go:generate go run ./wrap ccip BurnToAddressMintTokenPool burn_to_address_mint_token_pool
+//go:generate go run ./wrap ccip BurnMintFastTransferTokenPool fast_transfer_token_pool
+//go:generate go run ./wrap ccip CCTPMessageTransmitterProxy cctp_message_transmitter_proxy
+//go:generate go run ./wrap ccip SiloedUSDCTokenPool siloed_usdc_token_pool
+//go:generate go run ./wrap ccip USDCTokenPoolCCTPV2 usdc_token_pool_cctp_v2
+//go:generate go run ./wrap ccip USDCTokenPool usdc_token_pool
+//go:generate go run ./wrap ccip BurnMintWithLockReleaseFlagTokenPool burn_mint_with_lock_release_flag_token_pool
+
+// Helpers
+//go:generate go run ./wrap ccip MaybeRevertMessageReceiver maybe_revert_message_receiver
+//go:generate go run ./wrap ccip LogMessageDataReceiver log_message_data_receiver
+//go:generate go run ./wrap ccip PingPongDemo ping_pong_demo
+//go:generate go run ./wrap ccip MessageHasher message_hasher
+//go:generate go run ./wrap ccip MultiOCR3Helper multi_ocr3_helper
+//go:generate go run ./wrap ccip USDCReaderTester usdc_reader_tester
+//go:generate go run ./wrap ccip ReportCodec report_codec
+//go:generate go run ./wrap ccip EtherSenderReceiver ether_sender_receiver
+//go:generate go run ./wrap ccip MockE2EUSDCTokenMessenger mock_usdc_token_messenger
+//go:generate go run ./wrap ccip MockE2EUSDCTransmitter mock_usdc_token_transmitter
+//go:generate go run ./wrap ccip MockE2ELBTCTokenPool mock_lbtc_token_pool
+//go:generate go run ./wrap ccip CCIPReaderTester ccip_reader_tester
+
+// EncodingUtils
+//go:generate go run ./wrap ccip EncodingUtils ccip_encoding_utils
+
+// Extract bytecode and ABI from generated wrappers
+//go:generate go run github.com/smartcontractkit/chainlink-evm/gethwrappers/helpers/extract_bytecode -input=generated -bytecode=../bytecode -abi=../abi
---
+++ lib/chainlink-ccip/deployment/tokens/configure_tokens_for_transfers.go
@@ -0,0 +1,207 @@
+package tokens
+
+import (
+	"fmt"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/ethereum/go-ethereum/common"
+	chain_selectors "github.com/smartcontractkit/chain-selectors"
+	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/changesets"
+	datastore_utils "github.com/smartcontractkit/chainlink-ccip/deployment/utils/datastore"
+	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/mcms"
+	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
+	"github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+	cldf_ops "github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	mcms_types "github.com/smartcontractkit/mcms/types"
+)
+
+// TokenTransferConfig specifies configuration for a token on one chain to enable transfers with other chains.
+type TokenTransferConfig struct {
+	// ChainSelector identifies the chain on which the token lives.
+	ChainSelector uint64
+	// TokenPoolRef is a reference to the token pool in the datastore.
+	// Populate the reference as needed to match the desired token pool.
+	TokenPoolRef datastore.AddressRef
+	// TokenRef is a reference to the token in the datastore. This is only needed if the token address cannot be derived from the pool reference.
+	TokenRef datastore.AddressRef
+	// ExternalAdmin is specified when we want to propose an admin that we don't control.
+	// Leave empty to use internal administration.
+	ExternalAdmin string
+	// RegistryRef is a reference to the contract on which the token pool must be registered.
+	// Populate the reference as needed to match the desired registry.
+	RegistryRef datastore.AddressRef
+	// RemoteChains specifies the remote chains to configure on the token pool.
+	RemoteChains map[uint64]RemoteChainConfig[*datastore.AddressRef, datastore.AddressRef]
+}
+
+// ConfigureTokensForTransfersConfig is the configuration for the ConfigureTokensForTransfers changeset.
+type ConfigureTokensForTransfersConfig struct {
+	// ChainAdapterVersion specifies the version of the chain adapter to use for this chain.
+	ChainAdapterVersion *semver.Version `yaml:"chainAdapterVersion" json:"chainAdapterVersion"`
+	// Tokens specifies the tokens to configure for cross-chain transfers.
+	Tokens []TokenTransferConfig
+	// MCMS configures the resulting proposal.
+	MCMS mcms.Input
+}
+
+// ConfigureTokensForTransfers returns a changeset that configures tokens on multiple chains for transfers with other chains.
+func ConfigureTokensForTransfers(tokenRegistry *TokenAdapterRegistry, mcmsRegistry *changesets.MCMSReaderRegistry) cldf.ChangeSetV2[ConfigureTokensForTransfersConfig] {
+	return cldf.CreateChangeSet(makeApply(tokenRegistry, mcmsRegistry), makeVerify(tokenRegistry, mcmsRegistry))
+}
+
+func makeVerify(_ *TokenAdapterRegistry, _ *changesets.MCMSReaderRegistry) func(cldf.Environment, ConfigureTokensForTransfersConfig) error {
+	return func(e cldf.Environment, cfg ConfigureTokensForTransfersConfig) error {
+		// TODO: implement
+		return nil
+	}
+}
+
+func makeApply(tokenRegistry *TokenAdapterRegistry, mcmsRegistry *changesets.MCMSReaderRegistry) func(cldf.Environment, ConfigureTokensForTransfersConfig) (cldf.ChangesetOutput, error) {
+	return func(e cldf.Environment, cfg ConfigureTokensForTransfersConfig) (cldf.ChangesetOutput, error) {
+		configs := make(map[uint64]TokenTransferConfig, len(cfg.Tokens))
+		for _, config := range cfg.Tokens {
+			configs[config.ChainSelector] = config
+		}
+		batchOps, reports, err := processTokenConfigForChain(e, configs, cfg.ChainAdapterVersion)
+		if err != nil {
+			return cldf.ChangesetOutput{}, fmt.Errorf("failed to process token configs for chains: %w", err)
+		}
+		return changesets.NewOutputBuilder(e, mcmsRegistry).
+			WithReports(reports).
+			WithBatchOps(batchOps).
+			Build(cfg.MCMS)
+	}
+}
+
+func processTokenConfigForChain(e deployment.Environment, cfg map[uint64]TokenTransferConfig, chainAdapterVersion *semver.Version) ([]mcms_types.BatchOperation, []cldf_ops.Report[any, any], error) {
+	tokenRegistry := GetTokenAdapterRegistry()
+	batchOps := make([]mcms_types.BatchOperation, 0)
+	reports := make([]cldf_ops.Report[any, any], 0)
+
+	for selector, token := range cfg {
+		tokenPool, err := datastore_utils.FindAndFormatRef(e.DataStore, token.TokenPoolRef, selector, datastore_utils.FullRef)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to resolve token pool ref on chain with selector %d: %w", selector, err)
+		}
+		registry, err := datastore_utils.FindAndFormatRef(e.DataStore, token.RegistryRef, selector, datastore_utils.FullRef)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to resolve registry ref on chain with selector %d: %w", selector, err)
+		}
+
+		family, err := chain_selectors.GetSelectorFamily(selector)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to get chain family for chain selector %d: %w", selector, err)
+		}
+		adapter, ok := tokenRegistry.GetTokenAdapter(family, chainAdapterVersion)
+		if !ok {
+			return nil, nil, fmt.Errorf("no token adapter registered for chain family '%s' and chain adapter version '%s'", family, chainAdapterVersion)
+		}
+
+		remoteChains := make(map[uint64]RemoteChainConfig[[]byte, string], len(token.RemoteChains))
+		for remoteChainSelector, inCfg := range token.RemoteChains {
+			remoteFamily, err := chain_selectors.GetSelectorFamily(remoteChainSelector)
+			if err != nil {
+				return nil, nil, fmt.Errorf("failed to get chain family for remote chain selector %d: %w", remoteChainSelector, err)
+			}
+			remoteAdapter, ok := tokenRegistry.GetTokenAdapter(remoteFamily, chainAdapterVersion)
+			if !ok {
+				return nil, nil, fmt.Errorf("no token adapter registered for chain family '%s' and chain adapter version '%s'", remoteFamily, chainAdapterVersion)
+			}
+			counterpart, ok := cfg[remoteChainSelector]
+			if !ok {
+				return nil, nil, fmt.Errorf("missing token transfer config for remote chain selector %d", remoteChainSelector)
+			}
+			counterpartRemoteChainCfg, ok := counterpart.RemoteChains[selector]
+			if !ok {
+				return nil, nil, fmt.Errorf("missing remote chain config for chain selector %d in token transfer config for remote chain selector %d", selector, remoteChainSelector)
+			}
+			remoteChains[remoteChainSelector], err = convertRemoteChainConfig(e, remoteAdapter, selector, remoteChainSelector, inCfg, counterpartRemoteChainCfg.OutboundRateLimiterConfig)
+			if err != nil {
+				return nil, nil, fmt.Errorf("failed to process remote chain config for remote chain selector %d: %w", remoteChainSelector, err)
+			}
+		}
+
+		configureTokenReport, err := cldf_ops.ExecuteSequence(e.OperationsBundle, adapter.ConfigureTokenForTransfersSequence(), e.BlockChains, ConfigureTokenForTransfersInput{
+			ChainSelector:     selector,
+			TokenPoolAddress:  tokenPool.Address,
+			RemoteChains:      remoteChains,
+			ExternalAdmin:     token.ExternalAdmin,
+			RegistryAddress:   registry.Address,
+			TokenRef:          token.TokenRef,
+			PoolType:          tokenPool.Type.String(),
+			ExistingDataStore: e.DataStore,
+		})
+		if err != nil {
+			return batchOps, reports, fmt.Errorf("failed to configure token pool on chain with selector %d: %w", selector, err)
+		}
+
+		batchOps = append(batchOps, configureTokenReport.Output.BatchOps...)
+		reports = append(reports, configureTokenReport.ExecutionReports...)
+	}
+	return batchOps, reports, nil
+}
+
+func convertRemoteChainConfig(
+	e deployment.Environment,
+	adapter TokenAdapter,
+	chainSelector uint64,
+	remoteChainSelector uint64,
+	inCfg RemoteChainConfig[*datastore.AddressRef, datastore.AddressRef],
+	chainSelectorOutboundTprl RateLimiterConfigFloatInput,
+) (RemoteChainConfig[[]byte, string], error) {
+	// a chain's inbound rate limiter config should be based on the remote chain's outbound rate limiter config
+	// to ensure that the remote chain is configured to allow the desired traffic from this chain.
+	// The values here should NOT be passed in decimal adjusted but rather the adapters should be responsible for performing
+	// any necessary decimal adjustments based on the token decimals on each chain.
+	outCfg := RemoteChainConfig[[]byte, string]{
+		InboundRateLimiterConfig:  chainSelectorOutboundTprl,
+		OutboundRateLimiterConfig: inCfg.OutboundRateLimiterConfig,
+	}
+	if inCfg.RemotePool != nil {
+		fullRemotePoolRef, err := datastore_utils.FindAndFormatRef(e.DataStore, *inCfg.RemotePool, remoteChainSelector, datastore_utils.FullRef)
+		if err != nil {
+			return outCfg, fmt.Errorf("failed to resolve remote pool ref %s: %w", datastore_utils.SprintRef(*inCfg.RemotePool), err)
+		}
+		outCfg.RemotePool, err = adapter.AddressRefToBytes(fullRemotePoolRef)
+		if err != nil {
+			return outCfg, fmt.Errorf("failed to convert remote pool ref %s to bytes: %w", datastore_utils.SprintRef(*inCfg.RemotePool), err)
+		}
+		// Can either provide the token reference directly or derive it from the pool reference.
+		if inCfg.RemoteToken != nil {
+			outCfg.RemoteToken, err = datastore_utils.FindAndFormatRef(e.DataStore, *inCfg.RemoteToken, remoteChainSelector, adapter.AddressRefToBytes)
+			if err != nil {
+				return outCfg, fmt.Errorf("failed to resolve remote token ref %s: %w", datastore_utils.SprintRef(*inCfg.RemoteToken), err)
+			}
+		} else {
+			outCfg.RemoteToken, err = adapter.DeriveTokenAddress(e, remoteChainSelector, fullRemotePoolRef)
+			if err != nil {
+				return outCfg, fmt.Errorf("failed to get remote token address via pool ref (%s) for remote chain selector %d: %w", datastore_utils.SprintRef(*inCfg.RemotePool), remoteChainSelector, err)
+			}
+		}
+		outCfg.RemoteToken = common.LeftPadBytes(outCfg.RemoteToken, 32)
+		outCfg.RemoteDecimals, err = adapter.DeriveTokenDecimals(e, remoteChainSelector, fullRemotePoolRef, outCfg.RemoteToken)
+		if err != nil {
+			return outCfg, fmt.Errorf("failed to get remote token decimals for remote chain selector %d: %w", remoteChainSelector, err)
+		}
+		outCfg.RemotePool, err = adapter.DeriveTokenPoolCounterpart(e, remoteChainSelector, outCfg.RemotePool, outCfg.RemoteToken)
+		if err != nil {
+			return outCfg, fmt.Errorf("failed to derive remote pool counterpart for remote chain selector %d: %w", remoteChainSelector, err)
+		}
+	}
+	for _, ccvRef := range inCfg.OutboundCCVs {
+		fullCCVRef, err := datastore_utils.FindAndFormatRef(e.DataStore, ccvRef, chainSelector, datastore_utils.FullRef)
+		if err != nil {
+			return outCfg, fmt.Errorf("failed to resolve outbound CCV ref %s: %w", datastore_utils.SprintRef(ccvRef), err)
+		}
+		outCfg.OutboundCCVs = append(outCfg.OutboundCCVs, fullCCVRef.Address)
+	}
+	for _, ccvRef := range inCfg.InboundCCVs {
+		fullCCVRef, err := datastore_utils.FindAndFormatRef(e.DataStore, ccvRef, chainSelector, datastore_utils.FullRef)
+		if err != nil {
+			return outCfg, fmt.Errorf("failed to resolve inbound CCV ref %s: %w", datastore_utils.SprintRef(ccvRef), err)
+		}
+		outCfg.InboundCCVs = append(outCfg.InboundCCVs, fullCCVRef.Address)
+	}
+	return outCfg, nil
+}
